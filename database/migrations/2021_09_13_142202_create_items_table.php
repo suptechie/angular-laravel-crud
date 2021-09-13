cba586defc0974c58aa391e1b392fe3abc7bd5cf
suptechie <suptechie@outlook.com>
2021-09-13 20:19:23 +0530

    :sparkles: Create items table migration file

---
+++ database/migrations/2021_09_13_142202_create_items_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements();
+            $table->string('name');
+            $table->string('quantity');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
