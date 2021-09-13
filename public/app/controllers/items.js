d0e660c29323000fe14751373d3d5aa9f24a01ea
suptechie <suptechie@outlook.com>
2021-09-13 21:59:23 +0530

    :sparkles: Create function add item, edit item & delete item

--- public/app/controllers/items.js
+++ public/app/controllers/items.js
@@ -11,10 +11,81 @@ app.controller('itemsController', function ($scope, $http, API_URL) {
         url: API_URL + "items"
     }).then(function (response) {
         $scope.items = response.data.items;
-        console.log(response);
     }, function (error) {
         console.log(error);
         alert('This is embarassing. An error has occurred. Please check the log for details');
     });
-    
+
+    //show modal form
+    $scope.toggle = function (modalstate, id) {
+        $scope.modalstate = modalstate;
+        $scope.item = null;
+
+        switch (modalstate) {
+            case 'add':
+                $scope.form_title = "Add New Item";
+                break;
+            case 'edit':
+                $scope.form_title = "Item Detail";
+                $scope.id = id;
+                $http.get(API_URL + 'items/' + id)
+                    .then(function (response) {
+                        console.log(response);
+                        $scope.item = response.data.item;
+                    });
+                break;
+            default:
+                break;
+        }
+
+        console.log(id);
+        $('#myModal').modal('show');
+    }
+
+    //save new record and update existing record
+    $scope.save = function (modalstate, id) {
+        var url = API_URL + "items";
+        var method = "POST";
+
+        //append item id to the URL if the form is in edit mode
+        if (modalstate === 'edit') {
+            url += "/" + id;
+
+            method = "PUT";
+        }
+
+        $http({
+            method: method,
+            url: url,
+            data: $.param($scope.item),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).then(function (response) {
+            console.log(response);
+            location.reload();
+        }), (function (error) {
+            console.log(error);
+            alert('This is embarassing. An error has occurred. Please check the log for details');
+        });
+    }
+
+    //delete record
+    $scope.confirmDelete = function (id) {
+        var isConfirmDelete = confirm('Are you sure you want this record?');
+        if (isConfirmDelete) {
+
+            $http({
+                method: 'DELETE',
+                url: API_URL + 'items/' + id
+            }).then(function (response) {
+                console.log(response);
+                location.reload();
+            }, function (error) {
+                console.log(error);
+                alert('Unable to delete');
+            });
+        } else {
+            return false;
+        }
+    }
+
 });
